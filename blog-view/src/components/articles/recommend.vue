72ce1cc335dad540851b2ae4431202b9dd47c1cf
LiLiLuL <1354044126@qq.com>
2019-05-06 14:12:13 +0800

    增加博文推荐页面

--- blog-view/src/components/articles/recommend.vue
+++ blog-view/src/components/articles/recommend.vue
@@ -1,13 +1,47 @@
 <template>
     <div>
-        这是文章推荐页面
+        <h1>博文推荐</h1>
+        <ul>
+            <li v-for="(item,index) in recommendList" :key="index">
+               {{index+1}}. <a :href="item.url">{{item.title}}</a>
+               <p class="recommend"><b>简评：</b>{{item.summary}}</p>
+            </li>
+        </ul>
     </div>
 </template>
 <script>
+import {recommendApi} from '../.././apiJS/api'
 export default {
+    data(){
+        return {
+             recommendList:''
+        }
+    },
+    methods:{
+        async init(){         
+            let data=await recommendApi.getAll();
+            this.recommendList=data.data;
+            console.log(this.recommendList);
+        }
+    },
+    created(){
+       this.init();
+    },
     
 }
 </script>
 <style scoped>
-
+h1{
+    border-left:10px solid rgb(255, 91, 118);
+    padding-left:20px;
+    color:rgb(255, 91, 118);
+    font: outline;
+}
+li{
+    margin-top:10px;
+}
+.recommend{
+    color:gray;
+    font-size:14px;
+}
 </style>
