f4a3156efa975c628b56efeab4c6d4629247b273
LiLiLuL <1354044126@qq.com>
2019-05-06 14:52:06 +0800

    实现多个分类下查找相同的文章的搜索功能

--- blog-view/src/components/articles/category.vue
+++ blog-view/src/components/articles/category.vue
@@ -7,27 +7,57 @@
             v-for="(tag,index) in categories"
             type="info" 
             class="tag"
+            @click="checkCategory(tag.id,index)"
             >
             {{tag.name}}
+             <p v-show="show(index)" class="check"></p>
+         
             </el-tag>
-            <el-button type="default">搜索</el-button>
+            <el-button type="default" @click="search(checkedCategories)">搜索</el-button>
         </ul>
         <hr />
-        <el-card class="box-card" 
-                :key="index"  
-                v-for="(card,index)  in categoryView"   
-                v-show="card.categoryArticles"    
-                shadow="hover">
-            <div slot="header" class="clearfix">
-                <span><b>{{card.name}}</b></span>                   
+        <div v-if="showList">
+            <el-card class="box-card" 
+                    :key="index"  
+                    v-for="(card,index)  in categoryView"   
+                    v-show="card.categoryArticles"    
+                    shadow="hover">
+                <div slot="header" class="clearfix">
+                    <span><b>{{card.name}}</b></span>                   
+                </div>
+                <ul>
+                    <li v-for="(o,index) in card.categoryArticles" :key="index" class="textItem">   
+                        {{index+1+"."}} {{ o.title }}
+                    </li>
+                </ul>
+                <p class="findMore"><el-button  v-show="card.categoryArticles"  class="find-more-button" type="text" @click="findMore(card.name)" >查看更多>>></el-button></p>
+            </el-card>
+        </div>
+        <div v-else>
+            <div v-show="theSameArticle!=null">
+                <div v-for="(item,index) in theSameArticle" :key="index" class="sameArticle">
+                
+                    <div class="right">
+                        <img :src="'/server/'+item.pictureUrl" alt=""/>
+                    </div>
+                    <div class="left">
+                        <h2>{{index+1}}.{{item.title}}</h2>
+                            <p><b>摘要：</b>{{item.summary}}</p>
+                        <ul class="information">
+                            <li>发表时间：{{item.create_by}} </li>
+                            <li>阅读：{{item.hits}}</li>
+                        </ul>
+                        
+                    </div>
+                </div>
             </div>
-            <ul>
-                <li v-for="(o,index) in card.categoryArticles" :key="index" class="textItem">   
-                    {{index+1+"."}} {{ o.title }}
-                </li>
-            </ul>
-            <p class="findMore"><el-button  v-show="card.categoryArticles"  class="find-more-button" type="text" @click="findMore(card.name)" >查看更多>>></el-button></p>
-        </el-card>
+            <div v-show="theSameArticle==null">
+                 
+                <img src="../../assets/img/cry.jpg" alt="" style="width:400px;height:400px;float:left;margin-top:-50px;"/>
+                <h3>未查找到符合您搜索条件的文章！</h3>
+                 
+            </div>
+        </div>
     </div>
 </template>
 <script>
@@ -37,14 +67,18 @@ export default {
         return{
             categories:[],
             categoryView:[],
-            showCard:true
+            showCard:true,
+            showCheck:[],
+            checkedCategories:[],
+            showList:true,
+            showSearch:false,
+            theSameArticle:[]
         }
     },
     methods:{
         getAllCategories(){
             categoryApi.allCategory().then(res=>{
                 this.categories=res.data.data;
-                console.log("111")
                 let category=res.data.data;
                 let all=category.map(ele => {
                     let obj={
@@ -63,7 +97,41 @@ export default {
         },
         findMore(name){
              this.$router.push({path:"/index/categoryArticles",query:{name:name}});
-         }
+         },
+        checkCategory(id,index){
+           
+            let flag=this.showCheck.indexOf(index);
+            if(flag==-1){
+                this.showCheck.push(index);
+                this.checkedCategories.push(id);
+
+            }else{
+                this.showCheck.splice(flag,1);
+                this.checkedCategories.splice(flag,1);
+            }
+             console.log(this.checkedCategories);
+        },
+        show(index){
+            let flag=this.showCheck.indexOf(index);
+            if(flag==-1){
+                return false;
+            }
+            return true;
+            
+        },
+        async search(categories){
+            
+            if(categories.length==0){
+                this.showList=true;
+            }else{
+                categories=categories.join(",");
+                console.log(categories);
+                let data=await categoryApi.search(categories);
+                this.theSameArticle=data.data;
+                this.showList=false;
+            }
+
+        }
     },
     created(){
         this.getAllCategories();
@@ -75,11 +143,23 @@ export default {
 .tagDiv,h1{
     text-align: left;
 }
+
 h1{
     border-left:10px solid rgb(255, 91, 118);
     padding-left:20px;
     color:rgb(255, 91, 118);
     font: outline;
+}
+h2{
+    font-size:18px;
+}
+h3{
+    font-size:4rem;
+    height: 400px;
+    margin-left:410px;
+    text-align: center;
+    /* color:rgb(241, 173, 14); */
+    color:rgba(5, 51, 104, 0.548);
 }
 .tag{
     margin:10px;
@@ -129,5 +209,59 @@ h1{
 }
 .clearfix{
     text-align: center;
+}
+
+
+.check::after{
+    content: '√';
+    color:#fff;
+    height: 10px;
+    width:10px;
+    font-size:12px;
+    position:absolute;
+    top:-15px;
+    left:-4px;
+}
+.check{
+    border:8px solid rgba(223, 41, 125, 0.747);
+    width: 0;
+    height:0;
+    border-radius: 50%;
+    position:relative;
+    float:right;
+    margin-top:16px;
+    margin-right:-4px;
+}
+.left{
+    height:156px;
+    margin-right:240px;   
+}
+.right{
+    width:240px;
+    height:150px;
+    float: right;
+    border:2px solid saddlebrown;
+
+}
+.right img{
+    width:100%;
+    height:100%;
+}
+.information{
+    display: flex;
+    justify-content: space-around;
+    font-size:14px;
+    position:absolute;
+    float: right;
+    /* width:100%; */
+    width:500px;
+    position: relative;
+    bottom:-60px;
+    right:0;
+    
+}
+.sameArticle{
+    border-bottom:2px solid brown;
+    margin-top:8px;
 }
 </style>
